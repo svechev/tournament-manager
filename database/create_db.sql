1ea6a4e618cecc72979fe3cb2ec36d29d6fc869b
Iliyan Svechev <iliansvechev@gmail.com>
2025-12-29 17:37:18 +0200

    Добавих скрипт за създаване на базата данни
    Включва трите таблици User, Tournament и Participates.
    Трябва в бъдеще да се добави и Bracket.

---
+++ database/create_db.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE IF NOT EXISTS tournament_db
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+USE tournament_db;
+
+CREATE TABLE User (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(63) NOT NULL UNIQUE,
+    email VARCHAR(63) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
+CREATE TABLE Tournament (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(63) NOT NULL,
+    description VARCHAR(255),
+    category VARCHAR(31),
+    start_datetime DATETIME NOT NULL,
+    end_datetime DATETIME NOT NULL,
+    status ENUM('upcoming', 'ongoing', 'finished') NOT NULL DEFAULT 'upcoming',
+    capacity INT UNSIGNED NOT NULL,
+    spots_taken INT UNSIGNED NOT NULL DEFAULT 0, 
+    --spots_taken might not be needed but I added it for now, needs to be updated every time
+    is_team_based BOOLEAN NOT NULL,
+    creator_user_id INT UNSIGNED NOT NULL,
+
+    CONSTRAINT fk_tournament_creator
+        FOREIGN KEY (creator_user_id)
+        REFERENCES User(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+) ENGINE=InnoDB;
+
+CREATE TABLE Participates (
+    user_id INT UNSIGNED NOT NULL,
+    tournament_id INT UNSIGNED NOT NULL,
+    team_name VARCHAR(63) NOT NULL,
+    result_position INT UNSIGNED, --type for this is not set in stone
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY(user_id, tournament_id),
+
+    CONSTRAINT fk_tournament_id
+        FOREIGN KEY (tournament_id)
+        REFERENCES Tournament(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES User(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE     
+);
